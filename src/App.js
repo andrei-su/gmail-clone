a3b5bb7380f1c6903d630000007e52c9a8db4c8c
andrei <git1@email.com>
2021-10-26 11:03:49 +0200

    Adding Router and routes
    * Adding Router and react-router-dom dependency + the Switch and the routes for the main home page and the route for opening each individual e-mail

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+// React Router
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 // Components
 import Header from './components/Header/Header';
 import Sidebar from './components/Sidebar/Sidebar';
@@ -8,12 +10,20 @@ import './App.css';
 function App() {
   return (
     // BEM naming convention
+    <Router>
       <div className="app">
         <Header />
         <div className="app__body">
           <Sidebar />
+          <Switch>
+            <Route path="/mail">
+            </Route>
+            <Route path="/">
+            </Route>
+          </Switch>
         </div>
       </div>
+    </Router>
   );
 }
 
