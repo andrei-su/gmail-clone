4edda97f182e05139cad0f44d5b6ca81542ddd6c
andrei <git1@email.com>
2021-10-26 10:38:40 +0200

    Added SidebarOption component

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,9 +1,16 @@
 import React from 'react';
 // Components
 import { Button } from '@material-ui/core';
+import SidebarOption from '../SidebarOption/SidebarOption';
 // Icons
 import {
   Add,
+  ExpandMore,
+  Inbox,
+  LabelImportant,
+  NearMe,
+  Note,
+  Star,
 } from '@material-ui/icons';
 // Styles
 import './Sidebar.css';
@@ -14,6 +21,12 @@ function Sidebar() {
       <Button startIcon={<Add fontSize="large" />} className="sidebar__compose">
         Compose
       </Button>
+      <SidebarOption Icon={Inbox} title="Inbox" number={54} selected={true} />
+      <SidebarOption Icon={Star} title="Stared" number={2} />
+      <SidebarOption Icon={LabelImportant} title="Important" number={2} />
+      <SidebarOption Icon={NearMe} title="Sent" number={2} />
+      <SidebarOption Icon={Note} title="Drafts" number={2} />
+      <SidebarOption Icon={ExpandMore} title="More" number={2} />
     </div>
   );
 }
