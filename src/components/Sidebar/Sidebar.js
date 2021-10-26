c68fb82034dd1ef6bbe0ec4f2e38e9dc2f589040
andrei <git1@email.com>
2021-10-26 10:47:00 +0200

    Added Sidebar Footer section

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,15 +1,18 @@
 import React from 'react';
 // Components
-import { Button } from '@material-ui/core';
+import { Button, IconButton } from '@material-ui/core';
 import SidebarOption from '../SidebarOption/SidebarOption';
 // Icons
 import {
   Add,
+  Duo,
   ExpandMore,
   Inbox,
   LabelImportant,
   NearMe,
   Note,
+  Person,
+  Phone,
   Star,
 } from '@material-ui/icons';
 // Styles
@@ -27,6 +30,20 @@ function Sidebar() {
       <SidebarOption Icon={NearMe} title="Sent" number={2} />
       <SidebarOption Icon={Note} title="Drafts" number={2} />
       <SidebarOption Icon={ExpandMore} title="More" number={2} />
+
+      <div className="sidebar__footer">
+        <div className="sidebar__footerIcons">
+          <IconButton>
+            <Person />
+          </IconButton>
+          <IconButton>
+            <Duo />
+          </IconButton>
+          <IconButton>
+            <Phone />
+          </IconButton>
+        </div>
+      </div>
     </div>
   );
 }
