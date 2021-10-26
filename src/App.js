3116e120a8f0fd22b9bb840cfac7ced50b200cce
andrei <git1@email.com>
2021-10-26 12:18:53 +0200

    Added Mail and EmailList empty components

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 // Components
 import Header from './components/Header/Header';
 import Sidebar from './components/Sidebar/Sidebar';
+import Mail from './components/Mail/Mail';
+import EmailList from './components/EmailList/EmailList';
 // Styles
 import './App.css';
 
@@ -17,8 +19,10 @@ function App() {
           <Sidebar />
           <Switch>
             <Route path="/mail">
+              <Mail />
             </Route>
             <Route path="/">
+              <EmailList />
             </Route>
           </Switch>
         </div>
---
+++ src/components/Mail/Mail.js
@@ -0,0 +1,12 @@
+import React from "react";
+// Styles
+import "./Mail.css";
+
+function Mail() {
+  return (
+    <div className="mail">
+    </div>
+  );
+}
+
+export default Mail;
