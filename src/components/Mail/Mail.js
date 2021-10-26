61f9b802cbac48b8af1f00b8328902cb7fc501ab
andrei <git1@email.com>
2021-10-26 16:08:43 +0200

    Added Mail top header

--- src/components/Mail/Mail.js
+++ src/components/Mail/Mail.js
@@ -1,10 +1,57 @@
 import React from "react";
+// Components
+import { IconButton } from "@material-ui/core";
+// Icons
+import {
+  ArrowBack,
+  CheckCircle,
+  Delete,
+  Email,
+  Error,
+  LabelImportant,
+  MoreVert,
+  MoveToInbox,
+  WatchLater,
+} from "@material-ui/icons";
 // Styles
 import "./Mail.css";
 
 function Mail() {
   return (
     <div className="mail">
+      <div className="mail__tools">
+        <div className="mail__toolsLeft">
+          <IconButton>
+            <ArrowBack />
+          </IconButton>
+          <IconButton>
+            <MoveToInbox />
+          </IconButton>
+          <IconButton>
+            <Error />
+          </IconButton>
+          <IconButton>
+            <Delete />
+          </IconButton>
+          <IconButton>
+            <Email />
+          </IconButton>
+          <IconButton>
+            <WatchLater />
+          </IconButton>
+          <IconButton>
+            <CheckCircle />
+          </IconButton>
+          <IconButton>
+            <LabelImportant />
+          </IconButton>
+          <IconButton>
+            <MoreVert />
+          </IconButton>
+        </div>
+        <div className="mail__toolsRight">
+        </div>
+      </div>
     </div>
   );
 }
