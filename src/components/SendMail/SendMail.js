57d027d25cc42c45a9820247e5e938d3e3299436
andrei <git1@email.com>
2022-02-08 15:23:50 +0100

    SendMain Convert Indentation to Tabs

--- src/components/SendMail/SendMail.js
+++ src/components/SendMail/SendMail.js
@@ -6,33 +6,33 @@ import "./SendMail.css";
 import { Button } from "@material-ui/core";
 
 function SendMail() {
-  return (
-    <div className="sendMail">
-      <div className="sendMail__header">
-        <h3>New Message</h3>
-        <Close className="sendMail__close" />
-      </div>
-      <form>
-        <input type="text" placeholder="To" />
-        <input type="text" placeholder="Subject" />
-        <input
-          type="text"
-          placeholder="Message..."
-          className="sendMail__message"
-        />
-        <div className="sendMail__options">
-          <Button
-            className="sendMail__send"
-            variant="contained"
-            color="primary"
-            type="submit"
-          >
-            Send
-          </Button>
-        </div>
-      </form>
-    </div>
-  );
+	return (
+		<div className="sendMail">
+			<div className="sendMail__header">
+				<h3>New Message</h3>
+				<Close className="sendMail__close" />
+			</div>
+			<form>
+				<input type="text" placeholder="To" />
+				<input type="text" placeholder="Subject" />
+				<input
+					type="text"
+					placeholder="Message..."
+					className="sendMail__message"
+				/>
+				<div className="sendMail__options">
+					<Button
+						className="sendMail__send"
+						variant="contained"
+						color="primary"
+						type="submit"
+					>
+						Send
+					</Button>
+				</div>
+			</form>
+		</div>
+	);
 }
 
 export default SendMail;
